6c3c6306ff8e9b297c2a6523879bff35680759e1
Alexander Deider <arth.medintech@gmail.com>
2020-10-07 21:07:10 +0700

    range() vector generator

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
-#include <iostream>
-using std::cout;
+#include <cstdio>
+#include <vector>
+using std::vector;
 
-int main(){
-	cout << "ok\n";
+vector<int> range(int start, int stop, int step = 1) {
+    vector<int> numbers;
+
+    for (auto i = start; i < stop; i += step) {
+        numbers.push_back(i);
+    }
+
+    return numbers;
+}
+vector<int> range(int stop) {return range(0, stop, 1); }
+
+int main() {
+    const auto numbers = range(-10, 11);
+    for (auto n : numbers) {
+    	printf("%3d ", n);
+    }
+	printf("\n\n");
+ 
+    for (auto n : range(100)) {
+    	printf("%3d ", n);
+    	if ((n+1) % 20 == 0) printf("\n");
+    }
+	printf("\n");
 }
